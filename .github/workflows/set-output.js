df3b31b563d275ceb0094d33d684b3994fb199a2
Wu-Yijun <wuyijun21@mails.ucas.ac.cn>
2024-04-13 14:42:01 -0700

    Refactor updateReleaseNotes function and update release body and changelog content in test_get_msg.yaml workflow

---
+++ .github/workflows/set-output.js
@@ -0,0 +1,4 @@
+const fs = require('fs').promises;
+const CHANGELOG_FILE = 'CHANGELOG.md';
+const CHANGELOG_CONTENT = fs.readFileSync(CHANGELOG_FILE, 'utf8');
+console.log(`::set-output name=content::${JSON.stringify(CHANGELOG_CONTENT)}`);
