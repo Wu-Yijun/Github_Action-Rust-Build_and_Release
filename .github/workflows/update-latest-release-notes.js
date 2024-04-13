c3326b8b6020d69ed4fa9c6670bf6c1011779ff0
Wu-Yijun <wuyijun21@mails.ucas.ac.cn>
2024-04-13 12:21:42 -0700

    Refactor updateReleaseNotes function in update-latest-release-notes.js

--- .github/workflows/update-latest-release-notes.js
+++ .github/workflows/update-latest-release-notes.js
@@ -1,24 +1,29 @@
-// 获取传递的参数
-// path
-const args = process.argv.slice(6);
-console.log('传递的参数：', args);
-const [token, owner, repo, release_id, tag_name, release_info] = args;
 
-// Octokit.js
-// https://github.com/octokit/core.js#readme
-const {promises: fs} = require('fs')
-const octokit = new Octokit({auth: token});
+async function updateReleaseNotes(
+    path, token, owner, repo, release_id, tag_name, release_info) {
+  // 获取传递的参数
+  const args = process.argv.slice(6);
+  const [token, owner, repo, release_id, tag_name, release_info] = args;
 
-const content = await fs.readFile(path, 'utf8');
+  // Octokit.js
+  // https://github.com/octokit/core.js#readme
+  const {promises: fs} = require('fs')
+  const octokit = new Octokit({auth: token});
 
-await octokit.request(`PATCH /repos/${owner}/${repo}/releases/${release_id}`, {
-  owner: owner,
-  repo: repo,
-  release_id: release_id,
-  tag_name: tag_name,
-  target_commitish: 'master',
-  body: '### ' + release_info + '\n\n' + content,
-  draft: false,
-  prerelease: false,
-  headers: {'X-GitHub-Api-Version': '2022-11-28'}
-});
+  const content = await fs.readFile(path, 'utf8');
+
+  await octokit.request(
+      `PATCH /repos/${owner}/${repo}/releases/${release_id}`, {
+        owner: owner,
+        repo: repo,
+        release_id: release_id,
+        tag_name: tag_name,
+        target_commitish: 'master',
+        body: '### ' + release_info + '\n\n' + content,
+        draft: false,
+        prerelease: false,
+        headers: {'X-GitHub-Api-Version': '2022-11-28'}
+      });
+}
+
+updateReleaseNotes(...process.argv.slice(2)).catch(console.error);
