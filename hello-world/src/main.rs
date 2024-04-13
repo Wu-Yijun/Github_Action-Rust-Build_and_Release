8eea1babaea023ae0a489891fc670caf723128e4
Wu-Yijun <wuyijun21@mails.ucas.ac.cn>
2024-04-13 02:22:52 -0700

    Refactor GitHub Actions workflow to update release asset paths and include counter and upload URL

--- hello-world/src/main.rs
+++ hello-world/src/main.rs
@@ -1,4 +1,6 @@
 fn main() {
     println!("Hello, world!");
-    println!("Hello, world2!");
+    let lib = unsafe { libloading::Library::new("libs/library_test.dll").unwrap() };
+    let add: libloading::Symbol<fn(usize, usize) -> usize> = unsafe { lib.get(b"add\0").unwrap() };
+    println!("Test ffi: 1 + 2 = {}", add(1, 2));
 }
