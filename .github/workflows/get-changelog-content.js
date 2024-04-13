d65af88e6c507d2506da37d30683affc5f6e65f3
Wu-Yijun <wuyijun21@mails.ucas.ac.cn>
2024-04-13 14:44:03 -0700

    Refactor updateReleaseNotes function and update changelog content in get-changelog-content.js

--- .github/workflows/get-changelog-content.js
+++ .github/workflows/get-changelog-content.js
@@ -1,4 +1,4 @@
-const fs = require('fs').promises;
+const fs = require('fs');
 const CHANGELOG_FILE = 'CHANGELOG.md';
 const CHANGELOG_CONTENT = fs.readFileSync(CHANGELOG_FILE, 'utf8');
 console.log(`::set-output name=content::${JSON.stringify(CHANGELOG_CONTENT)}`);
