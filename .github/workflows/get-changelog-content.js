e958886a4e94fface203a7f6515c8d529d51fc06
Wu-Yijun <wuyijun21@mails.ucas.ac.cn>
2024-04-13 14:55:06 -0700

    Update changelog content in get-changelog-content.js and test_get_msg.yaml

--- .github/workflows/get-changelog-content.js
+++ .github/workflows/get-changelog-content.js
@@ -1,4 +1,23 @@
+const {execSync} = require('child_process');
+
+let content = '';
+try {
+  // 执行 git log 命令获取最近一次提交的提交消息
+  const COMMIT_MESSAGE = execSync('git log -1 --pretty=%B').toString().trim();
+  // 分段, 每段的两端加上斜体 * *
+  content = content +
+      COMMIT_MESSAGE.split('\n').map(line => `### *${line}*`).join('\n');
+} catch (error) {
+  console.error('Error getting commit message:', error);
+}
+
+// 读取 CHANGELOG.md 文件的内容
 const fs = require('fs');
 const CHANGELOG_FILE = 'CHANGELOG.md';
-const CHANGELOG_CONTENT = fs.readFileSync(CHANGELOG_FILE, 'utf8');
-console.log(`::set-output name=content::${JSON.stringify(CHANGELOG_CONTENT)}`);
+try {
+  const CHANGELOG_CONTENT = fs.readFileSync(CHANGELOG_FILE, 'utf8');
+  content = content + CHANGELOG_CONTENT;
+} catch (error) {
+  console.error('Error reading CHANGELOG.md:', error);
+}
+console.log(`::set-output name=content::${JSON.stringify(content)}`);
