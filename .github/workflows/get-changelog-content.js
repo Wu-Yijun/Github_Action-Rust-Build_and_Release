bbf9a2d84a9e873a5ac69333b639b0e86fadc5f0
Wu-Yijun <wuyijun21@mails.ucas.ac.cn>
2024-04-13 15:02:45 -0700

    Update changelog content and generate diff in get-changelog-content.js and test_get_msg.yaml

--- .github/workflows/get-changelog-content.js
+++ .github/workflows/get-changelog-content.js
@@ -11,6 +11,8 @@ try {
   console.error('Error getting commit message:', error);
 }
 
+content += '\n\n';
+
 // 读取 CHANGELOG.md 文件的内容
 const fs = require('fs');
 const CHANGELOG_FILE = 'CHANGELOG.md';
@@ -20,4 +22,16 @@ try {
 } catch (error) {
   console.error('Error reading CHANGELOG.md:', error);
 }
+
+content += '\n\n## *Diff*:\n\n';
+
+// 读取commit_diff_temp.md文件的内容
+const COMMIT_DIFF_FILE = 'commit_diff_temp.md';
+try {
+  const COMMIT_DIFF_CONTENT = fs.readFileSync(COMMIT_DIFF_FILE, 'utf8');
+  content = content + COMMIT_DIFF_CONTENT;
+} catch (error) {
+  console.error('Error reading commit_diff_temp.md:', error);
+}
+
 console.log(`::set-output name=content::${JSON.stringify(content)}`);
