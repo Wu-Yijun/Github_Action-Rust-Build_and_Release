102eb84d29e273185986dbe716858121e11638c0
Wu-Yijun <wuyijun21@mails.ucas.ac.cn>
2024-04-14 18:18:29 -0700

    测试 ffmpeg 环境搭建

--- hello-world/src/main.rs
+++ hello-world/src/main.rs
@@ -1,34 +1,12 @@
-#[cfg(target_os = "windows")]
-const PLATFORM: &str = "Windows";
-#[cfg(target_os = "windows")]
-const LIBRART_PATH: &str = "libs/library_test.dll";
 
-#[cfg(target_os = "linux")]
-const PLATFORM: &str = "Linux";
-#[cfg(target_os = "linux")]
-const LIBRART_PATH: &str = "libs/liblibrary_test.so";
-
-#[cfg(target_os = "macos")]
-const PLATFORM: &str = "macOS";
-#[cfg(target_os = "macos")]
-const LIBRART_PATH: &str = "libs/liblibrary_test.dylib";
-
-const TEST_ERROR_PATH: &str = "libs/libtest_error";
+mod test_lib_load;
+mod test_video;
 
 fn main() {
-    // std::panic::set_hook(Box::new(|err| {
-    //     println!("Panic occurred");
-    //     println!("{}", err);
-    //     std::process::exit(1);
-    // }));
-
     println!("Hello, world!");
-    println!("Running on {}", PLATFORM);
-    // let lib = unsafe { libloading::Library::new(TEST_ERROR_PATH).unwrap() };
-    let lib = unsafe { libloading::Library::new(LIBRART_PATH).unwrap() };
-    let add: libloading::Symbol<fn(usize, usize) -> usize> = unsafe { lib.get(b"add\0").unwrap() };
-    println!("Test ffi: 1 + 2 = {}", add(1, 2));
-    assert_eq!(999 + 111, add(999, 111));
+
+    // test_lib_load::main();
+    test_video::main();
 
     // If everything is fine, exit with a zero status code
     std::process::exit(0);
